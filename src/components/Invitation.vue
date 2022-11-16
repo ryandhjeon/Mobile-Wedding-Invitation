075146748d333be06c1a9362e6923fe94c7852b2
Ryan Jeon <ryandhjeon@gmail.com>
2022-11-16 00:43:42 -0500

    Edit main story

--- src/components/Invitation.vue
+++ src/components/Invitation.vue
@@ -7,7 +7,7 @@
         </div> -->
         
         <div class="story2">
-            각자의 길을 열심히 걸어온 저희가, <br /> 이제는 손잡고 함께 발 맞춰가는 새로운 여정을 시작합니다. <br /> 사랑하는 이 사람과, <br /> 있는 힘을 다해 멋있고 행복한 여정을 만들어 나가겠습니다. <br /> 뜻깊은 날 함께 축복해 주시면 더 없는 기쁨으로 간직하겠습니다.
+            각자의 길을 열심히 걸어온 저희가 <br /> 이제는 손잡고 함께 발 맞춰가는 새로운 여정을 시작합니다. <br /> 사랑하는 이 사람과 있는 힘을 다해 <br /> 멋있고 행복한 여정을 만들어 나가겠습니다. <br /> 뜻깊은 날 함께 축복해 주시면 <br /> 더 없는 기쁨으로 간직하겠습니다.
         </div> 
         <div class="directory">
             <div><span class="bold">전인영 & 이희순</span> 의 장남 <span class="bold">동헌</span></div>
@@ -19,7 +19,7 @@
 <style> 
 .directory {
     text-align: center;
-    margin: 3em 0;
+    margin-top: 5em;
 }
 .story1 {
     text-align: center;
