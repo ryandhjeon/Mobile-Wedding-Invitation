d702a26f65eeb2bea3ea09ada12596c7fcaf238b
Ryan Jeon <ryandhjeon@gmail.com>
2022-11-18 16:47:07 -0500

    Add Locale

--- src/components/Locale.vue
+++ src/components/Locale.vue
@@ -1,15 +1,22 @@
 <template>
-    <el-select class="locale-changer" v-model="$i18n.locale" placeholder="Language" size="small">
+    <!-- <el-select class="locale-changer" v-model="$i18n.locale" placeholder="Language" size="small">
         <el-option 
             v-for="item in langs" 
             :key="item.value" 
             :label="item.label"
             :value="item.value"
         />
-    </el-select>
+    </el-select> -->
+    <div class="lang_selector">
+        <el-radio-group class="lang_button" v-model="$i18n.locale" @click="isActive = !isActive">
+            <el-radio-button :value='en' :label="true">ENG</el-radio-button>
+        </el-radio-group>
+    </div>
+
 </template>
 
 <script>
+
 export default {
   name: 'locale-changer',
   data () {
@@ -30,13 +37,13 @@ export default {
 </script>
 
 <style scoped>
-.locale-changer {
+
+.lang_selector {
     position: absolute;
     top: 5px;
-    right: 5px;
+    left: 5px;
     z-index: 999;
     width: 4em;
     box-shadow: 0;
-
 }
 </style>
