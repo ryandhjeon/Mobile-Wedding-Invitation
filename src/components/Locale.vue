4045396352497ed2ef120a6d66e95f92358009e7
Ryan Jeon <ryandhjeon@gmail.com>
2022-11-18 21:22:14 -0500

    Add Locale

--- src/components/Locale.vue
+++ src/components/Locale.vue
@@ -1,24 +1,18 @@
 <template>
-    <!-- <el-select class="lang_selector" v-model="$i18n.locale" placeholder="Language" size="small">
-        <el-option 
+    <select class="lang_selector" v-model="$i18n.locale" placeholder="Language" size="small">
+        <option 
             v-for="item in langs" 
             :key="item.value" 
             :label="item.label"
             :value="item.value"
         />
-    </el-select> -->
+    </select>
 
-    <div class="lang_selector">
+    <!-- <div class="lang_selector">
         <el-radio-group v-model="$i18n.locale">
-            <el-radio-button v-model="$i18n.locale" 
-                v-for="item in langs" 
-                :label="item.label" 
-                :value="item.value" 
-                :key="item.value">
-                {{item.label}}
-            </el-radio-button>
+            <el-radio-button :label="en">ENG</el-radio-button>
         </el-radio-group>
-    </div>
+    </div> -->
 
 </template>
 
@@ -51,8 +45,16 @@ export default {
     top: 0.5em;
     left: 0.5em;
     z-index: 999;
-    width: 3em;
+    /* width: 3em; */
     box-shadow: 0;
+    /* background: transparent; */
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding: 0.7em;
+    background-color: black;
+    color: white;
+    border: 0;
 }
 
 </style>
